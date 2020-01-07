65e8827f37670170073e825fbe51df5d4c7cd78b
Travis Watson <watsonjamest@gmail.com>
2020-01-07 13:39:48 -0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center default-margin">
+                <div class="alert alert-info">Info: Click on a Quote to Delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+
+    export default {
+        data(){
+            return {
+                maxQuotes: 10,
+                quotes: ['Just a quote to see soemthing'],
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes){
+                    return alert ("Please delete quotes first.");
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+    .default-margin{
+        margin: 2% 0;
+    }
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row text-center">
+        <form>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 text-center">
+            <label class="default-margin">Enter Quote:</label>
+            <textarea rows="3" class="form-control default-margin" v-model="quote"></textarea>
+        </div>
+                <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 text-center">
+            <button class="btn btn-primary default-margin" @click.prevent="createNew">New Quote</button>
+        </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quote: ''
+            };
+        },
+        methods: {
+            createNew() {
+                this.$emit('quoteAdded', this.quote);
+                this.quote = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
+
